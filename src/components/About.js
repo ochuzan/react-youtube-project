9333f17109554f5d5ca6044eeaa8f553e3d514bd
David Vidals <david.vidals08@gmail.com>
2021-12-09 15:08:33 -0500

    David about Bio commit

--- src/components/About.js
+++ src/components/About.js
@@ -12,6 +12,11 @@ const About = () => {
                 Alex Tan: Full Stack Web Fellow at Pursuit, lived in Virginia before moving to New York to learn how to become a better coder.
                 <a href="https://github.com/AlexTan21">My GitHub</a>
             </p>
+            <br/>
+            <p>
+                David Vidals: Full Stack Web Fellow at Pursuit. Born and raised in NYC. I recommend search Bad Bunny using our YouTube page!
+                <a href="https://github.com/Davidvidals">My Github!</a>
+            </p>
         </header>
     );
 };
