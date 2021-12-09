55dc18dc079dadaa26bd4db8aa9f1978baf038d1
David Vidals <david.vidals08@gmail.com>
2021-12-09 15:58:18 -0500

    fixed up about css

--- src/components/About.js
+++ src/components/About.js
@@ -4,23 +4,27 @@ import Navbar from './Navbar';
 const About = () => {
     return (
         <header>
-            <Navbar />
+            
+            
+            <div id='about-display'>
+                <Navbar />
             <h1>
             Create a React application that lets you search for YouTube videos using the YouTube API, then choose a video from a list to watch.
             </h1>
-            <p>
+            <p class='alex'>
                 Alex Tan: Full Stack Web Fellow at Pursuit, lived in Virginia before moving to New York to learn how to become a better coder.
                 <a href="https://github.com/AlexTan21">My GitHub</a>
             </p>
             <br/>
-            <p>
+            <p class='david'>
                 David Vidals: Full Stack Web Fellow at Pursuit. Born and raised in NYC. I recommend searching Bad Bunny using our YouTube page!
                 <a href="https://github.com/Davidvidals">My Github!</a>
             </p>
-            <p>
+            <p class='oliver'>
                 Oliver Chuzan: Full Stack Web Fellow at Pursuit, formerly a Workday Financials Business Systems Associate
                 <a href="https://github.com/ochuzan">Oliver Chuzan's Github</a>
             </p>
+            </div>
         </header>
     );
 };
